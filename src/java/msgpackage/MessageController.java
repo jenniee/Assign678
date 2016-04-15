768703e427c98ac4af9f782591071e80438c0e16
c0637089 <c0637089@N208-F15-001.lambton.on.ca>
2016-04-15 15:01:15 -0400

    changes and messages added in Controller

--- src/java/msgpackage/MessageController.java
+++ src/java/msgpackage/MessageController.java
@@ -22,17 +22,13 @@ public class MessageController {
     private List<Message> messages = new ArrayList<>();
     public Message instance = new Message();
     
-    public void add(Message newMessage){
-        messages.add(newMessage);
-    }
-    
     public List<Message> getAllMessages(){
         DateFormat df = new SimpleDateFormat("yyyy/mm/dd HH:mm:ss");
         Date msgDate = instance.getSenttime();
         String stringDate = df.format(msgDate);
         return messages;
     }
-
+    
     public Message getMessageById(int id){
         for(Message selectedMessage : messages){
             if(selectedMessage.getId() == id){
@@ -42,12 +38,28 @@ public class MessageController {
         return null;
     }
     
-    public Message getMessageByDate(Date from, Date to){
-        for(Message selectedMessage : messages){
-            if(selectedMessage.getSenttime().after(from) && selectedMessage.getSenttime().before(to)){
-                return selectedMessage;
+   public List<Message> getMessageByDate(Date from, Date to){
+        for(Message msg : messages){
+            if(msg.getSenttime().after(from) && msg.getSenttime().before(to)){
+                return messages;
             }
         }
         return null;
     }
+   
+   public void add(Message msg){
+        messages.add(msg);
+    }
+   
+   public void edit(Message msg){
+       
+   }
+    
+    public void remove(int id){
+        
+    }
+
+    
+    
+    
 }
